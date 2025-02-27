c65f3d240d004123d2a8308a4603af9df7fdcdfb
Mohammad Amini <132151000+mohammadaminiaf@users.noreply.github.com>
2025-02-27 22:10:42 +0430

    Updated my example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 - Initial release of ReadMore package
 - Basic text truncation functionality
 - Customizable buttons and styles
+
+## 1.0.1
+
+- Added `blurredReadMore` extension method to add a "Read More" blur effect to text.
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
+import 'read_more.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: Text('Read More Example')),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ReadMoreExample(),
         ),
       ),
+    );
+  }
+}
+
+class ReadMoreExample extends StatefulWidget {
+  const ReadMoreExample({super.key});
+
+  @override
+  State<ReadMoreExample> createState() => _ReadMoreExampleState();
+}
+
+class _ReadMoreExampleState extends State<ReadMoreExample> {
+  bool isExpanded = false;
+
+  void toggleReadMore() {
+    setState(() {
+      isExpanded = !isExpanded;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'This is a long text that will be blurred if it exceeds the minimum lines. '
+      'Click the button below to read more or read less.',
+    ).blurredReadMore(
+      text:
+          'This is a long text that will be blurred if it exceeds the minimum lines. '
+          'Click the button below to read more or read less.',
+      isExpanded: isExpanded,
+      onToggle: toggleReadMore,
     );
   }
 }
